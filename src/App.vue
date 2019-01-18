65999f68d73da0bdc72d6558df59c8340be206f2
zhouhuiyan <15937367035@163.com>
2019-01-18 17:00:55 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <div>{{apijson}}</div>
+    <button @click="getname">get name</button>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -10,8 +12,24 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
+  data(){
+    return{
+      apijson:'我是初始数据'
+    }
+  },
   components: {
     HelloWorld
+  },
+  methods:{
+    getname(){
+      this.axios.get('http://localhost:8888/users/user').then((res)=>{
+        console.log(res.data);
+        this.apijson = res.data;
+      })
+      // .catch(err=>{
+      //   console.log(err);
+      // })
+    }
   }
 }
 </script>
