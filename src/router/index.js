6eb618d7ec2b67001d3fbc2093d8ba4f93180dbc
Unknown <15937367035@163.com>
2019-03-29 19:35:46 +0800

    add 404.vue

--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,10 @@ export default new VueRouter({
         requiresAuth:true,
       },
       component: () => import('../views/system/container/index'),
+    },
+    {
+      path:"*",
+      component: () => import('../views/public/404'),
     }
   ]
 })
