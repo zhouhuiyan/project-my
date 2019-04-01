16e3f547e372e93aec2dcf70f41f517b1169416f
Unknown <15937367035@163.com>
2019-04-01 10:51:05 +0800

    增加路由--占位置

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,61 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
-  routes:[
+  routes: [
     {
-      path:'/',
-      // component:resolve => 
-      component: () => import('../views/home/index'),
+      path: "/",
+      // component:resolve =>
+      component: () => import("../views/home/index")
     },
     {
-      path:'/system/login',
-      name:'system',
-      component: () => import('../views/system/login/index'),
+      path: "/system/login",
+      name: "system",
+      component: () => import("../views/system/login/index")
     },
     {
-      path:'/system',
-      name:'system',
-      meta:{
-        requiresAuth:true,
+      path: "/system",
+      name: "system",
+      meta: {
+        requiresAuth: true
       },
-      component: () => import('../views/system/container/index'),
+      component: () => import("../views/system/container/index"),
+      children: [
+        {
+          path: "/users",
+          component: () => import("../views/system/container/index"),
+          meta: {
+            requiresAuth: true
+          },
+        },
+        {
+          path: "/article",
+          component: () => import("../views/system/container/index"),
+          meta: {
+            requiresAuth: true
+          },
+        },
+        {
+          path: "/type",
+          component: () => import("../views/system/container/index"),
+          meta: {
+            requiresAuth: true
+          },
+        },
+        {
+          path: "/permissions",
+          component: () => import("../views/system/container/index"),
+          meta: {
+            requiresAuth: true
+          },
+        }
+      ]
     },
     {
-      path:"*",
-      component: () => import('../views/public/404'),
+      path: "*",
+      component: () => import("../views/public/404")
     }
   ]
-})
+});
