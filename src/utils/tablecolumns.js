9fb5b64c84923a286c32c4223763afdcc1c96431
Unknown <15937367035@163.com>
2019-04-03 14:38:10 +0800

    后台的列表页面的表格表头参数的提取

---
+++ src/utils/tablecolumns.js
@@ -0,0 +1,82 @@
+/**
+ * 表格数据测试
+ * @author zhy
+ */
+export const table=[
+  {
+    title: "Name",
+    key: "name"
+  },
+  {
+    title: "Title",
+    key: "title"
+  },
+  {
+    title: "Age",
+    key: "age"
+  },
+  {
+    title: "Action",
+    slot: "action"
+  }
+];
+
+/**
+ * 后台文章管理表格表头
+ * @author zhy 
+ */
+export const article=[
+  {
+    title: "Name",
+    key: "name"
+  },
+  {
+    title: "Name",
+    key: "name"
+  }
+];
+
+/**
+ * 后台用户管理表格表头
+ * @author zhy 
+ */
+export const users=[
+  {
+    title: "Name",
+    key: "name"
+  },
+  {
+    title: "Name",
+    key: "name"
+  }
+];
+
+/**
+ * 后台权限管理表格表头
+ * @author zhy 
+ */
+export const permissions=[
+  {
+    title: "Name",
+    key: "name"
+  },
+  {
+    title: "Name",
+    key: "name"
+  }
+];
+
+/**
+ * 后台分类管理表格表头
+ * @author zhy 
+ */
+export const type=[
+  {
+    title: "Name",
+    key: "name"
+  },
+  {
+    title: "Name",
+    key: "name"
+  }
+]
