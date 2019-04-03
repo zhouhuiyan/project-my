72940b42b961f28000939d156c7ce2b27c7004be
Unknown <15937367035@163.com>
2019-04-03 14:39:00 +0800

    增加公共组件ueditor组件

---
+++ src/components/ueditor/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <script id="editor" type="text/plain"></script>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    defaultMsg:{
+      type:String
+    },
+    config:{
+      type:Object
+    }
+  },
+  data(){
+    return{
+      editor:null,
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
